ce43fc02563f21d73436dc8b28b9f85921e54737
rui.mou <rui.mou@msn.cn>
2012-12-20 00:21:09 +0800

    [HEAD][H]update include/

--- include/linux/platform_data/tegra_usb.h
+++ include/linux/platform_data/tegra_usb.h
@@ -21,11 +21,29 @@ enum tegra_usb_operating_modes {
 	TEGRA_USB_OTG,
 };
 
+enum tegra_usb_phy_type {
+	TEGRA_USB_PHY_TYPE_UTMIP = 0,
+	TEGRA_USB_PHY_TYPE_LINK_ULPI = 1,
+	TEGRA_USB_PHY_TYPE_NULL_ULPI = 2,
+	TEGRA_USB_PHY_TYPE_HSIC = 3,
+	TEGRA_USB_PHY_TYPE_ICUSB = 4,
+};
+
 struct tegra_ehci_platform_data {
 	enum tegra_usb_operating_modes operating_mode;
 	/* power down the phy on bus suspend */
 	int power_down_on_bus_suspend;
+	int hotplug;
+	int default_enable;
 	void *phy_config;
+	enum tegra_usb_phy_type phy_type;
 };
+
+struct tegra_otg_platform_data {
+	struct platform_device *ehci_device;
+	struct tegra_ehci_platform_data *ehci_pdata;
+};
+
+void tegra_otg_check_vbus_detection();
 
 #endif /* _TEGRA_USB_H_ */
